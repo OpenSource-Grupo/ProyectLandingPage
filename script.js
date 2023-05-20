9e52ff70e66900925e226d563d5d38127329a667
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-05-20 12:22:58 -0500

    Final Version

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleContent(contentId) {
+    const contentSections = document.querySelectorAll('.content');
+  
+    contentSections.forEach(section => {
+      if (section.id === contentId) {
+        section.classList.add('active');
+      } else {
+        section.classList.remove('active');
+      }
+    });
+  }
+  
